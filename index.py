76cd76ad292e16af3b76e55a90802fe253b674c1
tomurashigaraki22 <125347750+tomurashigaraki22@users.noreply.github.com>
2023-12-02 15:02:46 +0000

    Update index.pys

--- index.py
+++ index.py
@@ -875,8 +875,10 @@ def push_to_github():
             # If 'origin' exists, remove it
             repo.git.remote('rm', 'origin') 
         # Add the new origin with the URL that includes the access token
-        repo.git.remote('add', 'origin', f"https://tomurashigaraki22:{access_token}@github.com/tomurashigaraki22/EcommerceServer.git")  # 
- 
+        repo.create_remote('origin', url=f"https://tomurashigaraki22:{access_token}@github.com/tomurashigaraki22/EcommerceServer.git")  # 
+        repo.git.config('user.email', 'emmanuelhudson355@gmail.com')  # Replace with your email
+        repo.git.config('user.name', 'tomurashigaraki22')  # Replace with your name
+
 
         # Add, commit, and push changes
         repo.git.add('.')
